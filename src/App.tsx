acf1f068c004bc8c9df8387f1dd23e2cde94d309
miyazi777 <m71203@gmail.com>
2023-06-10 18:18:24 +0900

    hoverイベント時の処理を追加

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,22 @@ function App() {
 
   useEffect(() => {
     const svgElement = d3.select(ref.current);
-    // cx:円の中心のx座標 cy:円の中心のy座標 r:円の半径
-    svgElement.append("circle").attr("cx", 120).attr("cy", 70).attr("r", 60);
+    svgElement
+      // cx:円の中心のx座標 cy:円の中心のy座標 r:円の半径
+      .append("circle")
+      .attr("cx", 120)
+      .attr("cy", 70)
+      .attr("r", 60)
+      // fill:塗りつぶしの色
+      .attr("fill", "rgba(0, 50, 250, 1.0)")
+      // mouseが重なった時の処理
+      .on("mouseover", function () {
+        d3.select(this).attr("opacity", "0.3");
+      })
+      // mouseが外れた時の処理
+      .on("mouseout", function () {
+        d3.select(this).attr("opacity", "1.0");
+      });
   }, []);
 
   return <svg ref={ref} />;
